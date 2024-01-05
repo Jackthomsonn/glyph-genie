0a3cb8176623f5e021f3a6da8faada9d1f3513d1
Jack Thomson <hello@jackthomson.co.uk>
2024-01-05 23:45:46 +0000

    upate

--- apps/glyph-genie-app/components/pricing.tsx
+++ apps/glyph-genie-app/components/pricing.tsx
@@ -1,10 +1,11 @@
+import { useUser } from "@/context/user";
 import { cn } from "@/lib/utils";
 import { loadStripe } from '@stripe/stripe-js';
 import axios from 'axios';
-import { BoxIcon, InfoIcon } from "lucide-react";
+import { BoxIcon } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
 import { Button } from "./ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
-import { useUser } from "@/context/user";
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
 
 const createCheckOutSession = async (userId?: string | null, price?: number) => {
   const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY || '');
@@ -24,6 +25,8 @@ const createCheckOutSession = async (userId?: string | null, price?: number) => 
 
 export const Pricing = () => {
   const { user } = useUser();
+  const pathName = usePathname();
+  const { push } = useRouter();
 
   return <div className="flex gap-6 flex-wrap flex-col md:flex-row">
     <Card className={cn("flex-1 h-auto")}>
@@ -32,9 +35,17 @@ export const Pricing = () => {
         <CardDescription>A mystical offering that grants you 10 wishes to be fulfilled by our elusive and all-powerful genies!</CardDescription>
       </CardHeader>
       <CardFooter>
-        <Button className="w-full bg-violet-800 text-white hover:bg-violet-900 h-full" onClick={() => createCheckOutSession(user?.id, 10)}>
-          <BoxIcon className="mr-2 h-4 w-4" /> Get started £10
-        </Button>
+        {
+          pathName === "/home"
+            ? <Button className="w-full bg-violet-800 text-white hover:bg-violet-900 h-full" onClick={() => push('dashboard')}>
+              <BoxIcon className="mr-2 h-4 w-4" /> Get started today
+            </Button>
+            :
+            <Button className="w-full bg-violet-800 text-white hover:bg-violet-900 h-full" onClick={() => createCheckOutSession(user?.id, 10)}>
+              <BoxIcon className="mr-2 h-4 w-4" /> Get started £10
+            </Button>
+        }
+
       </CardFooter>
     </Card>
 
@@ -44,9 +55,16 @@ export const Pricing = () => {
         <CardDescription>A wondrous trove of 50 wishes bestowed upon you, entrusted to our enigmatic and omnipotent genies!</CardDescription>
       </CardHeader>
       <CardFooter>
-        <Button className="w-full bg-violet-800 text-white hover:bg-violet-900 h-full" onClick={() => createCheckOutSession(user?.id, 50)}>
-          <BoxIcon className="mr-2 h-4 w-4" /> Get started £40
-        </Button>
+        {
+          pathName === "/home"
+            ? <Button className="w-full bg-violet-800 text-white hover:bg-violet-900 h-full" onClick={() => push('dashboard')}>
+              <BoxIcon className="mr-2 h-4 w-4" /> Get started today
+            </Button>
+            :
+            <Button className="w-full bg-violet-800 text-white hover:bg-violet-900 h-full" onClick={() => createCheckOutSession(user?.id, 50)}>
+              <BoxIcon className="mr-2 h-4 w-4" /> Get started £40
+            </Button>
+        }
       </CardFooter>
     </Card>
 
@@ -56,9 +74,16 @@ export const Pricing = () => {
         <CardDescription>A transcendental offering of 100 wishes bestowed upon you by our esteemed and omnipotent genies!</CardDescription>
       </CardHeader>
       <CardFooter>
-        <Button className="w-full bg-violet-800 text-white hover:bg-violet-900 h-full" onClick={() => createCheckOutSession(user?.id, 100)}>
-          <BoxIcon className="mr-2 h-4 w-4" /> Get started £80
-        </Button>
+        {
+          pathName === "/home"
+            ? <Button className="w-full bg-violet-800 text-white hover:bg-violet-900 h-full" onClick={() => push('dashboard')}>
+              <BoxIcon className="mr-2 h-4 w-4" /> Get started today
+            </Button>
+            :
+            <Button className="w-full bg-violet-800 text-white hover:bg-violet-900 h-full" onClick={() => createCheckOutSession(user?.id, 100)}>
+              <BoxIcon className="mr-2 h-4 w-4" /> Get started £80
+            </Button>
+        }
       </CardFooter>
     </Card>
   </div>
